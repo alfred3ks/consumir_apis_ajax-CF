6dcdf8339725ddc1d7e48e7886ac73596ede3dfc
Alfredo Sánchez <alfred3ks@gmail.com>
2022-04-02 21:02:23 +0200

    Encabezados permitidos.

---
+++ 12-politicas-cors/4-encabezados-permitidos/servidor/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cookieParser());
+
+let f = (req, res)=> {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send({
+        hola:"mundo"
+    });
+}
+
+let o = (req, res)=> {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "PUT");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authentication");
+    res.status(204).send()
+}
+
+app.options('/',o);
+
+app.get('/',f);
+app.post('/',f);
+app.put('/',f);
+
+app.listen(8081, ()=>console.log('Server ON'));
